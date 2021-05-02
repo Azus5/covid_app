054172f3278dce36530ded39a6a8aee029d88062
Abner <abnerchoba@hotmail.com>
2021-05-02 09:08:35 -0300

    Added font-awesome

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faSkull } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 25 }}>
+        C
+        <FontAwesomeIcon icon={faSkull} />
+        R
+        <FontAwesomeIcon icon={faSkull} />
+        NA
+        <Text style={{ color: "green" }}>VIRUS</Text>
+      </Text>
     </View>
   );
 }
